bdce9c98d3debefa97b5bb3a4b07d46882d04c88
Idir224 <idir.smail224@gmail.com>
2026-09-22 15:56:31 +0200

    Bonne version de l'exo 1

--- Exo1.java
+++ Exo1.java
@@ -1,16 +1,39 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * TPO Exercice 1
+ * 22 septembre 2026
+ * 
  */
 
-package exo1.exo1;
+package Exo1;
+
+import java.util.Scanner;
 
 /**
  *
- * @author USER
+ * @author Idir SMAIL
  */
 public class Exo1 {
 
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        System.out.println("Bonjour");
+
+        // Déclaration des variables
+        String prenom;
+        Scanner sc;
+
+        // Création du Scanner
+        sc = new Scanner(System.in);
+
+        // Demander prénom
+        System.out.println("Bonjour, quel est votre prenom ?");
+        prenom = sc.nextLine(); //Lit l'input de l'utilisateur
+
+        System.out.println("Bonjour " + prenom);
+
+        System.out.println("Au revoir");
     }
 }
